afe33ce18be4b91fd9b8aff722404714d143425b
Seang-G <mgw7730@gmail.com>
2024-05-29 17:15:19 +0900

    feat: add CreateListResponses

---
+++ src/main/java/com/maker/Smart_To_Do_List/response/CreateListResponse.java
@@ -0,0 +1,13 @@
+package com.maker.Smart_To_Do_List.response;
+
+import com.maker.Smart_To_Do_List.dto.ToDoListDto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreateListResponse extends BaseResponse<ToDoListDto>{
+
+}

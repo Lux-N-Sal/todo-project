a7450f6a5f3efe0cda1ccd0561b20d95f18a397e
Seang-G <mgw7730@gmail.com>
2024-05-31 14:51:14 +0900

    feat: add error codes
    - AE_001: Invalid Access
    - AE_002: Wrong Password
    - LIE_002: Invalid List Name

--- src/main/java/com/maker/Smart_To_Do_List/enums/ErrCode.java
+++ src/main/java/com/maker/Smart_To_Do_List/enums/ErrCode.java
@@ -7,6 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrCode {
     OK(""),     // ok
+    AE_001("Invalid Access"), // authorization: invalid access
+    AE_002("Wrong Password"), //authorization: invalid password
+
     JE_001("Duplicated ID"), // join: duplicated id
     JE_002("Invalid password check."), // join: invalid password. (폐기 예정)
     JE_003("Duplicated User Name"), //join: duplicated name
@@ -14,7 +17,8 @@ public enum ErrCode {
     LE_001("ID not found."), // login: id not found.
     LE_002("Wrong password."), // login: invalid password
 
-    LIE_001("Duplicated List Name"); // List: duplicated name
+    LIE_001("Duplicated List Name"), // list: duplicated name
+    LIE_002("Invalid List Name"); // list: invalid list name (ex. null)
 
     private final String error;
 }
